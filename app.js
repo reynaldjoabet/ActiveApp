91e3a75e38a643b9b8901955b41b1324aad545f5
reynaldjoabet <reynaldjoabet@gmail.com>
2021-11-03 18:32:01 +0400

    A complete structure with static files set and working

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const  express =require('express');
 const router=require('./routes/appRoutes');
+const path= require('path');
 const app= express();
 const port=process.env.PORT || 3000
+
+
+app.use(express.static('public')) // Serves resources from the public folder
 app.use('/',router)
+app.use('/css',express.static('public/css/'));
+app.use('/javascript',express.static('public/css/'));
+app.use('/images',express.static('public/css/'));
 
 app.listen(port,()=>{
     console.log(`Server started on port : ${port}`)
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,5 @@
 const path=require('path');
-const public= path.join(__dirname, '../public/');
-const express=require('express');
-const app=express();
-//app.use(express.static(public));
-app.use('/css', express.static('.'));
-//app.use('/javascript',express.static(path.join(public,'javascript/')));
-//console.log(path.join(public,'javascript/'))
-// Serves resources from the public folder
+// the homepage 
 exports.home=(req,res)=>{
     res.sendFile(path.join(__dirname,'../views/','index.html'))
 }
