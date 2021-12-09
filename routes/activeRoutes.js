e9eda2ba602d742b442b7b216c7df08461f9ccc5
Liyeuk Reynald <reynald@Liyeuks-MacBook-Air.local>
2021-12-10 02:53:37 +0400

    set up

--- routes/activeRoutes.js
+++ routes/activeRoutes.js
@@ -11,11 +11,11 @@ router.post('/active/signup',controller.signupPost); // used to collect the data
 router.get('/active/signup',controller.signupGet);// used to display the sign up form
 router.get('/active/dashboard',controller.dashboard);
 router.get('/active/dashboarddata',controller.dashboardData);
-router.put('/active/updateplan',controller.updatePlan);//edit trainging plan
+router.put('/active/updateplan',controller.updatePlan);    //edit trainging plan
 router.put('/active/updategoal',controller.updateGoal);
 router.delete('/active/deleteaccount',controller.deleteAccount);
 router.delete('/active/deletegoal',controller.deleteGoal);
-router.delete('/active/deleteplan',controller.deletePlan);
+//router.delete('/active/deleteplan',controller.deletePlan);
 router.get('/active/update',controller.dashboard);
 router.get('/active/createplan',controller.createPlanGet); //displays the create training plan form
 router.post('/active/createplan',controller.createPlanPost);// used to process the submitted data
